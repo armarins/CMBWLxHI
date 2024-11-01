51c5ab9a812400631ab3cecc611abd922a31ce39
armarins <alessandrormarins@gmail.com>
2024-11-01 22:25:55 +0800

    third update: including new ipynb, updating picture formatting, and noise function

--- scripts/noise_functions.py
+++ scripts/noise_functions.py
@@ -1,3 +1,4 @@
+import numpy as np
 def noise_parameters(nside=256, nu_min_MHz=980, nu_max_MHz=1260, nch=30, 
                      nbeams=28, Tsys=70, Osur=5324, Obeam=0.35, tsur_year=1, 
                      K=1.414  , fsky=0.13, dcycle=1, verbose=False, unit_mK=True):
@@ -57,4 +58,40 @@ def healpy_white_noise_Cl(nside=256, nu_min_MHz=980, nu_max_MHz=1260, nch=30,
                      nbeams=28, Tsys=70, Osur=5324, Obeam=0.35, tsur_year=1, 
                      K=1.414  , fsky=0.13, dcycle=1, verbose=False, unit_mK=True):
     return None
-    
+
+
+def Tsys_SKA_MID_band1(nu_eff=None):#[nu]=MHz. [Tsys]=K
+    Tcmb = 2.73#K
+    Tspl = 3.00#K
+    Tgal = 25*np.power(408/nu_eff,2.75) #K
+    Trx  = 15 + 30*np.power(nu_eff/1e3 - 0.75, 2) #K
+    Tsys = Tcmb+Tspl+Tgal+Trx
+    return Tsys
+
+
+def bl_function(fwhm=None, lmax=None, theta_=None, input_unit="radian", from_real_space=False): 
+    if input_unit=="radian":
+        pass
+    elif input_unit=="degree":
+        fwhm = np.radians(fwhm)
+        try:
+            theta_ = np.radians(theta_)
+        except:
+            pass
+    elif input_unit=="arcmin":
+        fwhm = np.radians(fwhm/60)
+        try:
+            theta_ = np.radians(theta_/60)
+        except:
+            pass
+    else:
+        pass   
+    if from_real_space:
+        bl  = model.beam_function(type_,fwhm=fwhm, theta_=theta_)
+        bl = hp.beam2bl(bl,theta_,lmax)    
+        bl = bl/bl.max()
+    else:
+        if type(lmax)==type(None):lmax=768
+        l  = np.arange(lmax+1)
+        bl = np.exp(-0.5*l*(l+1)*fwhm**2/8/np.sqrt(2))
+    return bl
